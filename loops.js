9da7f3abf2415404adeca49c3a24f59d08043418
Arthur Carson <carson.bob.arthur@gmail.com>
2017-12-31 23:06:48 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,21 @@
+
+
+function forLoop() {
+  for (let i = 0; i < 25; i++) {
+    if ( i != 1 ) {
+    x.push(`I am ${i} strange loops.`);
+    } else {
+    x.push("I am 1 strange loop.");
+    } 
+    return array;
+  }
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+  console.log(n);
+  n--;
+  }
+  return 'done';
+}
+
