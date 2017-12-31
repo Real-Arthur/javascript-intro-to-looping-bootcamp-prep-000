16129f8ca443bf86b2eba76e4753ed72726becf4
Arthur Carson <carson.bob.arthur@gmail.com>
2017-12-31 23:11:13 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@
 
 function forLoop(x) {
   for (let i = 0; i < 25; i++) {
-    if ( i != 1 ) {
+    if ( i !== 1 ) {
     return x.push(`I am ${i} strange loops.`);
     } else {
     return x.push("I am 1 strange loop.");
